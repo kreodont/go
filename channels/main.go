37a3f9725a38e27aa693c08577b4f2cd915728b8
kreodont <dimasanov@gmail.com>
2018-01-23 21:38:26 +0300

    Lesson 76

--- channels/main.go
+++ channels/main.go
@@ -8,16 +8,23 @@ import (
 func main() {
 	links := []string {"http://google.com", "http://facebook.com", "http://amazon.com", "http://golang.org",
 	"http://fakesite.ooooo"}
+	c := make(chan string)
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<- c)
 	}
 }
 
-func checkLink(link string)  {
+func checkLink(link string, channel chan string)  {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println("There is an issue with ", link)
+		channel <- "Might be down"
 		return
 	}
 	fmt.Println(link, "OK")
+	channel <- "Is up"
 }
